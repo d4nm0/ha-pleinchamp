0faeed5cc3fb0b46a43d6f6963206b1b03ececa3
Dan Monceau <dan.monceau.lpcb@gmail.com>
2026-05-15 13:54:25 +0200

    Update config_flow.py

--- custom_components/pleinchamp/config_flow.py
+++ custom_components/pleinchamp/config_flow.py
@@ -1,18 +1,25 @@
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
+
 from .const import DOMAIN, CONF_CITY_URL
 
 class PleinchampConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Gère le flux de configuration."""
+    """Gère le flux de configuration pour Pleinchamp."""
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Première étape de la configuration via l'interface."""
+        errors = {}
         if user_input is not None:
+            # Ici on pourrait ajouter une vérification de l'URL si besoin
             return self.async_create_entry(title="Pleinchamp Météo", data=user_input)
 
-        return self.self.async_show_form(
+        return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required(CONF_CITY_URL): str,
+                vol.Required(CONF_CITY_URL): cv.string,
             }),
+            errors=errors,
         )
