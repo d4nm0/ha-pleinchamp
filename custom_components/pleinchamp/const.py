692ef8da2744188d4da75cbd46a8cda15ea5cc10
Dan Monceau <dan.monceau.lpcb@gmail.com>
2026-05-15 21:39:02 +0200

    Update constants for pleinchamp component

--- custom_components/pleinchamp/const.py
+++ custom_components/pleinchamp/const.py
@@ -1,4 +1,5 @@
 DOMAIN = "pleinchamp"
-CONF_CITY_URL = "city_url"
-DEFAULT_SCAN_INTERVAL = 60 # minutes
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+CONF_LATITUDE = "latitude"
+CONF_LONGITUDE = "longitude"
+DEFAULT_SCAN_INTERVAL = 15
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
